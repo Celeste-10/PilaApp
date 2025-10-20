72d3b0968a2ae934ac0471ecd87ba68773c28006
AllisonF <allisonfitoria@uamv.edu.ni>
2025-10-20 09:29:21 -0600

    .

--- PilaApp/Form1.Designer.cs
+++ PilaApp/Form1.Designer.cs
@@ -28,13 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.tbApellido = new System.Windows.Forms.TextBox();
+            this.tbCargo = new System.Windows.Forms.TextBox();
+            this.tbSalario = new System.Windows.Forms.TextBox();
+            this.lbPila = new System.Windows.Forms.ListBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnVerTope = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombres:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Apellidos:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(67, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Cargo:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(67, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Salario:";
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(151, 44);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(176, 26);
+            this.tbNombre.TabIndex = 4;
+            // 
+            // tbApellido
+            // 
+            this.tbApellido.Location = new System.Drawing.Point(151, 97);
+            this.tbApellido.Name = "tbApellido";
+            this.tbApellido.Size = new System.Drawing.Size(176, 26);
+            this.tbApellido.TabIndex = 5;
+            // 
+            // tbCargo
+            // 
+            this.tbCargo.Location = new System.Drawing.Point(151, 150);
+            this.tbCargo.Name = "tbCargo";
+            this.tbCargo.Size = new System.Drawing.Size(176, 26);
+            this.tbCargo.TabIndex = 6;
+            // 
+            // tbSalario
+            // 
+            this.tbSalario.Location = new System.Drawing.Point(151, 193);
+            this.tbSalario.Name = "tbSalario";
+            this.tbSalario.Size = new System.Drawing.Size(176, 26);
+            this.tbSalario.TabIndex = 7;
+            // 
+            // lbPila
+            // 
+            this.lbPila.FormattingEnabled = true;
+            this.lbPila.ItemHeight = 20;
+            this.lbPila.Location = new System.Drawing.Point(379, 30);
+            this.lbPila.Name = "lbPila";
+            this.lbPila.Size = new System.Drawing.Size(335, 204);
+            this.lbPila.TabIndex = 8;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(32, 283);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(91, 48);
+            this.btnAgregar.TabIndex = 9;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(129, 283);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(91, 48);
+            this.btnEliminar.TabIndex = 10;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnVerTope
+            // 
+            this.btnVerTope.Location = new System.Drawing.Point(226, 283);
+            this.btnVerTope.Name = "btnVerTope";
+            this.btnVerTope.Size = new System.Drawing.Size(91, 48);
+            this.btnVerTope.TabIndex = 11;
+            this.btnVerTope.Text = "Ver Tope";
+            this.btnVerTope.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btnVerTope);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.lbPila);
+            this.Controls.Add(this.tbSalario);
+            this.Controls.Add(this.tbCargo);
+            this.Controls.Add(this.tbApellido);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Ingreso de empleados";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.TextBox tbApellido;
+        private System.Windows.Forms.TextBox tbCargo;
+        private System.Windows.Forms.TextBox tbSalario;
+        private System.Windows.Forms.ListBox lbPila;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnVerTope;
     }
 }
 
--- PilaApp/Form1.cs
+++ PilaApp/Form1.cs
@@ -12,9 +12,126 @@ namespace PilaApp
 {
     public partial class Form1 : Form
     {
+        public class Empleado
+        {
+            //Se declara la clase Empleado para almacenar los datos
+            public string Nombre { get; set; }
+            public string Apellidos { get; set; }
+            public string Cargo { get; set; }
+            public decimal Salario { get; set; }
+
+            public string NombreCompleto => $"{Nombre} {Apellidos}";
+
+            public override string ToString()
+            {
+                return $"{NombreCompleto}, {Cargo}, C${Salario:N2}";
+                //Formateamos el salario en cordobas, con dos decimales 
+            }
+        }
+        private Stack<Empleado> pila = new Stack<Empleado>(); //Declaramos la pila usando Stack
         public Form1()
         {
             InitializeComponent();
+
+            btnAgregar.Click += btnAgregar_Click;
+            btnEliminar.Click += btnEliminar_Click;
+            btnVerTope.Click += btnVerTope_Click;
+            //No me mostraba los empleados en la lista. Asi que agregamos esto.
         }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            //Validamos que los campos no esten vacios al ingresar los datos
+            if (string.IsNullOrWhiteSpace(tbNombre.Text) ||
+                string.IsNullOrWhiteSpace(tbApellido.Text) ||
+                string.IsNullOrWhiteSpace(tbCargo.Text) ||
+                string.IsNullOrWhiteSpace(tbSalario.Text))
+            {
+                MessageBox.Show("Advertencia: Todos los campos son obligatorios.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            //Validamos que el salario sea un numero valido y enviamos una advertencia si no lo es
+            if (!decimal.TryParse(tbSalario.Text, out decimal salario))
+            {
+                MessageBox.Show("Advertencia: El salario debe ser un número válido.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            //Creamos un nuevo empleado para seguir ingresando datos en la pila
+            Empleado nuevoEmpleado = new Empleado
+            {
+                Nombre = tbNombre.Text.Trim(),
+                Apellidos = tbApellido.Text.Trim(),
+                Cargo = tbCargo.Text.Trim(),
+                Salario = salario
+            };
+
+            pila.Push(nuevoEmpleado);
+            tbNombre.Clear();
+            tbApellido.Clear();
+            tbCargo.Clear();
+            tbSalario.Clear();
+            //Agregamos a la pila usando Push y limpiamos los TxtBox despues de agregar
+
+            ActualizarListBox();
+
+            //Para cada procedimiento realizado se actualiza la lista y mostramos un mensaje emergente de confirmacion
+            MessageBox.Show("Empleado agregado correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            //Verificamos la pila usando Count para evitar errores
+            if (pila.Count == 0)
+            {
+                MessageBox.Show("Advertencia: La pila está vacía.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            //Eliminamos el empleado usando Pop, se muestra el nombre completo en un mensaje emergente
+            Empleado eliminado = pila.Pop();
+
+            ActualizarListBox();
+
+            MessageBox.Show($"Empleado eliminado: {eliminado.NombreCompleto}", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnVerTope_Click(object sender, EventArgs e)
+        {
+            if (pila.Count == 0)
+            {
+                MessageBox.Show("Advertencia: La pila está vacía.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            Empleado tope = pila.Peek();
+
+            MessageBox.Show($"Tope de la pila: {tope.NombreCompleto}", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        //Utilizamos un metodo auxiliar para poder actualizar la lista
+        private void ActualizarListBox()
+        {
+            lbPila.Items.Clear();
+            Empleado[] array = pila.ToArray();
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                lbPila.Items.Add(array[i].ToString());
+            }
+        }
+        //El metodo convierte la pila a un arreglo con "array" y usando "for" que lo recorre en orden inverso
     }
+
 }
+
